1d2784c84bd1966c626272878ddb79b9af6d9581
aidancrowther <aidan.crowther@outlook.com>
2019-01-30 15:55:19 -0500

    Implemented all features, began writing tests

--- BST.go
+++ BST.go
@@ -1,58 +1,164 @@
 package main
 
+//Imports
 import(
 	"fmt"
+	"strconv"
+	"math/rand"
+	"time"
 )
 
+//Define node type
 type node struct{
 	parent *node;
 	leaves[2] *node;
-	data int;
+	key int;
+	val string;
 }
 
+//Define tree type
 type tree struct{
 	head *node;
 }
 
-func (t *tree) add(i int){
-	if t.head == nil {
-		t.head = &node{data: i};
-	} else {
-		t.head.add(i, nil);
-	}
-}
-
-func (n *node) add(i int, pN *node) *node{
+//Recursive add function
+func (n *node) add(key int, val string, pN *node) *node{
 	if n == nil {
-		return &node{data: i, parent: pN};
-	} else if i < n.data {
-		new := n.leaves[0].add(i, n);
+		//Base case, new node
+		return &node{key: key, val: val, parent: pN};
+	} else if key < n.key {
+		//Key is < current node key, call leaves[0]
+		new := n.leaves[0].add(key, val, n);
 		if new != nil { n.leaves[0] = new }
 		return nil;
-	} else if i > n.data {
-		new := n.leaves[1].add(i, n);
+	} else if key > n.key {
+		//Key is > current node key, call leaves[1]
+		new := n.leaves[1].add(key, val, n);
 		if new != nil { n.leaves[1] = new }
 		return nil;
+	} else {
+		//Base case, update key
+		n.val = val;
+		return nil;
 	}
-	return nil;
 }
 
-func (t *tree) print(){
+//Add key and val to tree, handling a new tree
+func (t *tree) add(key int, val string){
+	if t.head == nil {
+		t.head = &node{key: key, val: val};
+	} else {
+		t.head.add(key, val, nil);
+	}
+}
+
+//Recursively search the tree for a val given a key
+func (n *node) find(key int) string {
+	if n == nil { return "DNE"; }
+	if n.key < key { return n.leaves[1].find(key); }
+	if n.key > key { return n.leaves[0].find(key); }
+	return n.val;
+}
+
+//Find entry for node is the tree
+func (t *tree) find(key int) string {
 	if t.head != nil {
-		t.head.print();
+		return t.head.find(key);
+	}
+	return "DNE";
+}
+
+//Find the lowest value node below the current node
+func (n *node) findMin() *node {
+	curr := n;
+	for curr.leaves[0] != nil {
+		curr = curr.leaves[0];
+	}
+	return curr;
+}
+
+//Replace a given node with another one
+func (n *node) replace(new *node){
+	pN := n.parent;
+	if pN != nil {
+		if pN.leaves[0] == n {
+			pN.leaves[0] = new;
+		} else {
+			pN.leaves[1] = new;
+		}
+	}
+	if new != nil {
+		new.parent = pN;
 	}
 }
 
+//Recursively remove key value pair from the tree
+func (n *node) remove(key int){
+	if n == nil {
+		//If n is nil, cancel
+		return;
+	} else if n.key < key {
+		//Recursive call down the tree
+		n.leaves[1].remove(key);
+		return;
+	} else if n.key > key {
+		//Recursive call down the tree
+		n.leaves[0].remove(key);
+		return;
+	}
+	if n.leaves[0] != nil && n.leaves[1] != nil {
+		//Replace using the next appropriate element
+		new := n.leaves[1].findMin();
+		n.key = new.key;
+		n.val = new.val;
+		new.remove(new.key);
+	} else if n.leaves[1] == nil {
+		//Replace using the leftmost leaf
+		n.replace(n.leaves[0]);
+	} else if n.leaves[0] == nil {
+		//Replace using the rightmost leaf
+		n.replace(n.leaves[1]);
+	} else {
+		//Replace with nil
+		n.replace(nil);
+	}
+}
+
+//Remove key from tree
+func (t *tree) remove(key int){
+	if t.head != nil {
+		t.head.remove(key);
+	}
+}
+
+//Print the tree starting at the head
+func (t *tree) print(){
+	t.head.print();
+}
+
+//Recursively print all node key value pairs
 func (n *node) print(){
 	if n != nil {
 		n.leaves[0].print();
-		fmt.Println(n.data);
+		fmt.Println(n.key, ": ", n.val);
 		n.leaves[1].print();
 	}
 }
 
 func main() {
 	var t tree;
-	t.add(0);
-	t.print();
+	var strings[10000] string;
+
+	for i := 0; i < 10000; i++ {
+		strings[i] = strconv.Itoa(i);
+	}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+  	for _, i := range r.Perm(len(strings)) {
+    	t.add(i, strings[i]);
+	}
+	  
+	fmt.Println(t.find(9999));
+	fmt.Println(t.head.key);
+
 }
